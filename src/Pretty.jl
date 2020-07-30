eefbe92f8a7ed1cdd592882bba831772f8f40482
thautwarm <twshere@outlook.com>
2020-07-30 19:42:16 +0900

    use pprint v0.2

--- src/Pretty.jl
+++ src/Pretty.jl
@@ -2,4 +2,6 @@ Base.show(io::IO, v::LocalVar) = begin
     print(io, "LocalVar($(v.sym), is_mutable=$(v.is_mutable.x), is_shared=$(v.is_shared.x))")
 end
 
+PrettyPrint.is_atom_t(::Type{LocalVar}) = true
+
 Base.show(io::IO, scope :: Scope) = pprint(io, scope)
