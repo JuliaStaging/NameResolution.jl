80bfcddb0975135f66db664a13d42311e1a38129
Taine Zhao <thaut@logic.cs.tsukuba.ac.jp>
2020-07-30 19:25:59 +0900

    Update Pretty.jl

--- src/Pretty.jl
+++ src/Pretty.jl
@@ -1,15 +1,7 @@
-function PrettyPrint.pprint_impl(io, v::LocalVar, indent, newline)
-    print(io, "LocalVar($(v.sym), is_mutable=$(v.is_mutable.x), is_shared=$(v.is_shared.x))")
-end
-
-function PrettyPrint.pprint_impl(io, p::Pair, indent, newline)
-    pprint(io, p.first, indent, newline)
-    print(io, "=>")
-    pprint(io, p.second, indent + 2, false)
-end
-
-function PrettyPrint.pprint_impl(io, d::Dict, indent, newline)
-   PrettyPrint.pprint_for_seq(io, "{", "}", collect(d), indent, newline)
+function PrettyPrint.pp_impl(io, v::LocalVar, indent)
+    repr = "LocalVar($(v.sym), is_mutable=$(v.is_mutable.x), is_shared=$(v.is_shared.x))"
+    print(io, repr)
+    length(repr) + indent
 end
 
 Base.show(io::IO, scope :: Scope) = pprint(io, scope)
